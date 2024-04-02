9bc5171704ce0f810f6687d4db6bf72df12ef6de
KazbekMusaev <145585208+KazbekMusaev@users.noreply.github.com>
2024-04-02 21:49:00 +0300

    Изменение от Казбек

---
+++ TestProject/NewsController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsController.swift
+//  TestProject
+//
+//  Created by apple on 02.04.2024.
+//
+
+import UIKit
+
+final class NewsController: UIViewController {
+
+    //MARK: ViewLiffeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+        view.backgroundColor = .green
+    }
+
+}
