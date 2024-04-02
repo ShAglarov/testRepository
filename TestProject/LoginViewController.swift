e9cb54752867350be4e268b118ccdefb90bbb6ca
ShAglarov <shamil.aglarov@gmail.com>
2024-04-02 21:55:04 +0300

    created LoginViewController

---
+++ TestProject/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewsViewController.swift
+//  TestProject
+//
+//  Created by Shamil Aglarov on 02.04.2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    }
+}
